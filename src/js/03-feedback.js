4412146f5a75092d5bf72d5aca3e6c5f150edd0c
irensmile <curkan.ira@gmail.com>
2023-10-14 21:55:40 -0400

    refactoring

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,20 @@
 import throttle from "lodash.throttle"; 
 const form = document.querySelector(".feedback-form");
 
-const pizza = document.querySelector("input");
+const input = document.querySelector("input");
 const textarea = document.querySelector("textarea");
 
 const state = localStorage.getItem("feedback-form-state");
 
 if (state) {
-    pizza.value = JSON.parse(state).email;
+    input.value = JSON.parse(state).email;
     textarea.value = JSON.parse(state).message;
 }
 
 const submitCallback = () => {
-    console.log(pizza.value, textarea.value); 
+    console.log(input.value, textarea.value); 
     const info = {
-        email: pizza.value,
+        email: input.value,
         message: textarea.value
     }
     localStorage.setItem("feedback-form-state", JSON.stringify(info));
@@ -25,11 +25,11 @@ form.addEventListener("input", throttle(submitCallback, 500));
 form.addEventListener("submit", (event) => {
     localStorage.removeItem("feedback-form-state");
     console.log({
-        email: pizza.value,
+        email: input.value,
         message: textarea.value
     });
     form.reset();
-    // pizza.value = "";
+    // input.value = "";
     // textarea.value = "";
     event.preventDefault();
 })
